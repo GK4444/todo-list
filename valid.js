5064a803fa864ba359c91b9305f41610003e5ac7
GK4444 <gauravkavhar304@gmail.com>
2023-02-25 15:08:09 +0530

    test user added

--- valid.js
+++ valid.js
@@ -7,6 +7,14 @@ function validate(){
         window.location.replace('gk_kanban_index.html')
         return false;
     }
+    else if(password.value==='shahzad@1234' && username.value==='shahzad'){
+        window.location.replace('shahzad_index.html')
+        return false;
+    }
+    else if(password.value==='test@1234' && username.value==='test'){
+        window.location.replace('test_index.html')
+        return false;
+    }
     else{
         alert("Login Failed!!!!");
     }
